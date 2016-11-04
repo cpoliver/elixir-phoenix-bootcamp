71e1533a88f1834ee336a68e61d37d8e4ddf0184
Charles Oliver <cpoliver@gmail.com>
2016-11-04 19:05:54 +0000

    refactored Cards.load

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -26,11 +26,9 @@ defmodule Cards do
   end
 
   def load(filename) do
-     {status, binary} = File.read(filename)
-
-     case status do
-       :ok -> :erlang.binary_to_term binary
-       :error -> "Error: file '#{filename}' could not be found."
+     case File.read(filename) do
+       {:ok, binary} -> :erlang.binary_to_term binary
+       {:error, _reason } -> "Error: file '#{filename}' could not be found."
      end
   end
 end
