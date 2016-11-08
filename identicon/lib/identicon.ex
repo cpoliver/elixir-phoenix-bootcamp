2d321599f917012fab9a6d383ae0c6ae160fa8df
Charles Oliver <charles.oliver@sky.uk>
2016-11-08 09:58:12 +0000

    added first few methods for generating an identicon.  added image struct

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -1,2 +1,19 @@
 defmodule Identicon do
+  def main(input) do
+    input
+    |> hash_input
+    |> pick_colour
+  end
+
+  def hash_input(input) do
+    hex = :crypto.hash(:md5, input)
+    |> :binary.bin_to_list
+
+    %Identicon.Image{hex: hex}
+  end
+
+  def pick_colour(image) do
+    %Identicon.Image{hex: [r, g, b | _]} = image
+    [r, g, b]
+  end
 end
