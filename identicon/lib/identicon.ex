4cd7e443231119e47db25179d5eb842560e8fa26
Charles Oliver <charles.oliver@sky.uk>
2016-11-08 10:55:22 +0000

    finished writing image grid creation logic

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -4,11 +4,13 @@ defmodule Identicon do
     |> hash_input
     |> pick_colour
     |> build_grid
+    |> filter_odd_squares
   end
 
   def hash_input(input) do
-    hex = :crypto.hash(:md5, input)
-    |> :binary.bin_to_list
+    hex =
+      :crypto.hash(:md5, input)
+      |> :binary.bin_to_list
 
     %Identicon.Image{hex: hex}
   end
@@ -18,12 +20,25 @@ defmodule Identicon do
   end
 
   def build_grid(%Identicon.Image{hex: hex} = image) do
-    hex
-    |> Enum.chunk(3)
-    |> Enum.map(&mirror_row/1)
+    grid =
+      hex
+      |> Enum.chunk(3)
+      |> Enum.map(&mirror_row/1)
+      |> List.flatten
+      |> Enum.with_index
+
+     %Identicon.Image{image | grid: grid}
   end
 
   def mirror_row(row) do
      row ++ (row |> Enum.take(2) |> Enum.reverse)
+  end
+
+  def filter_odd_squares(%Identicon.Image{grid: grid} = image) do
+    filtered_grid = Enum.filter grid, fn({code, _}) ->
+      rem(code, 2) == 0
+    end
+
+    %Identicon.Image{image | grid: filtered_grid}
   end
 end
