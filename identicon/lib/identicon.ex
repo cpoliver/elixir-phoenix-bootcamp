9c22e99c37bc15711ff4cc9b533083570aca4d93
Charles Oliver <charles.oliver@sky.uk>
2016-11-08 10:39:59 +0000

    added method to generate matrix for creating identicon. added property to image struct to store this

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -3,6 +3,7 @@ defmodule Identicon do
     input
     |> hash_input
     |> pick_colour
+    |> build_grid
   end
 
   def hash_input(input) do
@@ -12,8 +13,17 @@ defmodule Identicon do
     %Identicon.Image{hex: hex}
   end
 
-  def pick_colour(image) do
-    %Identicon.Image{hex: [r, g, b | _]} = image
-    [r, g, b]
+  def pick_colour(%Identicon.Image{hex: [r, g, b | _]} = image) do
+    %Identicon.Image{image | colour: {r, g, b}}
+  end
+
+  def build_grid(%Identicon.Image{hex: hex} = image) do
+    hex
+    |> Enum.chunk(3)
+    |> Enum.map(&mirror_row/1)
+  end
+
+  def mirror_row(row) do
+     row ++ (row |> Enum.take(2) |> Enum.reverse)
   end
 end
